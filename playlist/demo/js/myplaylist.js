55f69f6dfa08eb925f7892436253a9ea8ecc5612
craneon <jennifer.friedmann@gmail.com>
2014-02-12 14:37:50 -0800

    Playlist: January 2014

---
+++ playlist/demo/js/myplaylist.js
@@ -0,0 +1,219 @@
+/**
+ * Credit to CodeBaseHero for structure.
+ * January 2014 Playlist - JF Compilation
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/01%20Part%20of%20Me%20Apart%20From%20You.mp3',
+        title:'Part of Me Apart From You',
+        artist:'Colin Stetson',
+        rating:5,
+        buy:'#',
+        price:'Salem',
+        duration:'5:51',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+        {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/02%20After%20the%20Job.mp3',
+        title:'After the Job',
+        artist:'Mix Mup',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'3.49',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/03%20Stop%20%28Imperialist%20Pop%29.mp3',
+        title:'Stop (Imperialist Pop)',
+        artist:'Atom TM',
+        rating:5,
+        buy:'#',
+        price:'Sausalito',
+        duration:'3.37',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/04%20Too%20Much%20%28Jessie%20Andrews%20%26%20Jason%20Burns%20Remix%29.mp3',
+        title:'Too Much (Jessie Andrews & Jason Burns Remix)',
+        artist:'Drake',
+        rating:5,
+        buy:'#',
+        price:'SF',
+        duration:'4:17',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+/**
+ *Track 5
+ */
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/05%20Buzzcut%20Season.m4a',
+        title:'Buzzcut Season',
+        artist:'Lorde',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'4:07',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/06%20Oh%20Darlin%27%20What%20Have%20I%20Done.mp3',
+        title:'Oh Darlin What Have I Done',
+        artist:'The White Buffalo',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'4:03',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/07%20If%20Not%20For%20You.mp3',
+        title:'If Not For You',
+        artist:'Bob Dylan',
+        rating:5,
+        buy:'#',
+        price:'NYC',
+        duration:'2:30',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/08%20That%27s%20Us%20_%20Wild%20Combination.mp3',
+        title:'Thats Us / Wild Combination',
+        artist:'Arthur Russell',
+        rating:5,
+        buy:'#',
+        price:'Oakland',
+        duration:'6:59',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/09%20Under%20Your%20Spell.mp3',
+        title:'Under Your Spell',
+        artist:'Desire',
+        rating:5,
+        buy:'#',
+        price:'Jerusalem',
+        duration:'4:56',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+/**
+* Track 10
+*/
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/10%20Shake%20It%20_%20Downtime%20In%20The%20Hangar.mp3',
+        title:'Shake It / Downtime in the Hangar',
+        artist:'DJ Deeon / Isomer Transitions',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'2:04',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/11%20Distant%20Stations.mp3',
+        title:'Distant Stations',
+        artist:'The Mountain Goats',
+        rating:5,
+        buy:'#',
+        price:'Singapore',
+        duration:'3:04',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/12%20Play%20by%20Play.m4a',
+        title:'Play By Play',
+        artist:'Autre Ne Veut',
+        rating:5,
+        buy:'#',
+        price:'Brooklyn',
+        duration:'5:14',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/13%20Cavalier.mp3',
+        title:'Cavalier',
+        artist:'James Vincent McMorrow',
+        rating:5,
+        buy:'#',
+        price:'Tahoe',
+        duration:'4:43',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/14%20Call%20Your%20Girlfriend.mp3',
+        title:'Call Your Girlfriend',
+        artist:'Robyn',
+        rating:5,
+        buy:'#',
+        price:'Durham',
+        duration:'3:47',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+/**
+* Track 15
+*/
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/15%20Lost%20At%20Sea%20%28Feat.%20Ryan%20Tedder%29.mp3',
+        title:'Lost At Sea (Feat. Ryan Tedder)',
+        artist:'Zedd',
+        rating:5,
+        buy:'#',
+        price:'NYC',
+        duration:'3:46',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/16%20Died%20In%20Your%20Arms.mp3',
+        title:'Died In Your Arms',
+        artist:'Bastille',
+        rating:5,
+        buy:'#',
+        price:'Boston',
+        duration:'3:21',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/17%20I%20Wanna%20Roo%20You%20%28Scottish%20Derivative%29.m4a',
+        title:'I Wanna Roo You (Scottish Derivative)',
+        artist:'Van Morrison',
+        rating:5,
+        buy:'#',
+        price:'New York',
+        duration:'3:27',
+        cover:'demo/mix/januaryiv.jpg'
+    },
+
+    {
+        mp3:'https://dl.dropboxusercontent.com/u/10691285/January%20IV/18%20A%20Santa%20Cecilia.mp3',
+        title:'A Santa Cecilia',
+        artist:'Gaelica',
+        rating:5,
+        buy:'#',
+        price:'Oxford',
+        duration:'5:56',
+        cover:'demo/mix/januaryiv.jpg'
+    }
+];
