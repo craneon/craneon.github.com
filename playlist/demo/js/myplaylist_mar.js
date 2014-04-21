d7f7c80458de6a0cf0bc4191a95af20ca8f75b65
craneon <jennifer.friedmann@gmail.com>
2014-04-20 23:24:18 -0700

    March Playlist

---
+++ playlist/demo/js/myplaylist_mar.js
@@ -0,0 +1,276 @@
+/**
+ * Credit to CodeBaseHero for structure.
+ * February 2014 Playlist - JF Compilation
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/01+Daylight+Again.mp3',
+        title:'Daylight Again',
+        artist:'Crosby, Stills, Nash',
+        rating:5,
+        buy:'#',
+        price:'Mission HS',
+        duration:'2:36',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+        {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/02+Don%27t+Wait.m4a',
+        title:'Don&#8217t Wait',
+        artist:'Mapei',
+        rating:5,
+        buy:'#',
+        price:'811 Page',
+        duration:'3:36',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/03+What%27s+A+Girl+To+Do.mp3',
+        title:'What&#8217s A Girl To Do',
+        artist:'Fatima Yamaha',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'7:27',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/04+Incognito.mp3',
+        title:'Incognito',
+        artist:'Vector Hold',
+        rating:5,
+        buy:'#',
+        price:'Tel Aviv',
+        duration:'4:33',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+/**
+ *Track 5
+ */
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/05+Philosophize+In+It!+Chemicalize+With+It!.mp3',
+        title:'Philosophize In It! Chemicalize With It!',
+        artist:'Kishi Bashi',
+        rating:5,
+        buy:'#',
+        price:'Cambridge',
+        duration:'3:31',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/06+Lose+It.mp3',
+        title:'Lose It',
+        artist:'Austra',
+        rating:5,
+        buy:'#',
+        price:'SF',
+        duration:'2:11',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/07+Mrs.+Morris.mp3',
+        title:'Mrs. Morris',
+        artist:'Charles Spearin',
+        rating:5,
+        buy:'#',
+        price:'Sausalito',
+        duration:'1:27',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/08+Rhythm+%26+Sound+-+No+Partial+(Kahn%27s+vocal+version).m4a',
+        title:'Rhythm & Sound - No Partial (Kahn vocal version)',
+        artist:'Kahn',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'4:46',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/09+A+Little+God+In+My+Hands.mp3',
+        title:'A Little God In My Hands',
+        artist:'Swans',
+        rating:5,
+        buy:'#',
+        price:'Seattle',
+        duration:'7:08',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+/**
+* Track 10
+*/
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/10+Kiss+Me.m4a',
+        title:'Kiss Me',
+        artist:'Ed Sheeran',
+        rating:5,
+        buy:'#',
+        price:'Newburgh',
+        duration:'5:26',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/11+Boomerang.mp3',
+        title:'Boomerang',
+        artist:'Manel',
+        rating:5,
+        buy:'#',
+        price:'Paris',
+        duration:'5:31',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/12+The+Grind+Date.mp3',
+        title:'The Grind Date',
+        artist:'De La Soul',
+        rating:5,
+        buy:'#',
+        price:'530 Bush',
+        duration:'3:23',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/13+One+Foot+in+the+Grave.m4a',
+        title:'One Foot in the Grave',
+        artist:'Head For THe Hills',
+        rating:5,
+        buy:'#',
+        price:'Portland',
+        duration:'4:00',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/14+Heart+in+Hand.mp3',
+        title:'Heart in Hand',
+        artist:'The Bookshop Band',
+        rating:5,
+        buy:'#',
+        price:'Oxford',
+        duration:'2:58',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+/**
+* Track 15
+*/
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/15+The+Gulf.m4a',
+        title:'The Gulf',
+        artist:'David Douglas',
+        rating:5,
+        buy:'#',
+        price:'Newark',
+        duration:'9:01',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/16+Tennessee.mp3',
+        title:'Tennessee',
+        artist:'Silver Jews',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'4:10',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/17+Little+Secrets.m4a',
+        title:'Little Secrets',
+        artist:'Passion Pit',
+        rating:5,
+        buy:'#',
+        price:'NYC',
+        duration:'3:59',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/18+we+are+the+sun+(original+mix).mp3',
+        title:'We Are The Sun (Original Mix)',
+        artist:'Savoy & Heather Bright',
+        rating:5,
+        buy:'#',
+        price:'LA',
+        duration:'5:24',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/19+Sonsick.mp3',
+        title:'Sonsick',
+        artist:'San Fermin',
+        rating:5,
+        buy:'#',
+        price:'NYC',
+        duration:'4:13',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+/**Track 20*/
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/20+It%27s+Nice+To+Be+Alive.mp3',
+        title:'It&#8217s Nice To Be Alive',
+        artist:'Ball Park Music',
+        rating:5,
+        buy:'#',
+        price:'NYC',
+        duration:'3:29',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/21+Whisky.mp3',
+        title:'Whisky',
+        artist:'Marian Hill',
+        rating:5,
+        buy:'#',
+        price:'Cambridge',
+        duration:'3:14',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/22+Sleep+Sound.mp3',
+        title:'Sleep Sound',
+        artist:'Jamie XX',
+        rating:5,
+        buy:'#',
+        price:'New Haven',
+        duration:'6:23',
+        cover:'demo/mix/marchiv.jpg'
+    },
+
+    {
+        mp3:'http://d1zehrk9wtqtav.cloudfront.net/23+Reservations.m4a',
+        title:'Reservations',
+        artist:'Wilco',
+        rating:5,
+        buy:'#',
+        price:'Brooklyn',
+        duration:'7:23',
+        cover:'demo/mix/marchiv.jpg'
+    }
+
+];
