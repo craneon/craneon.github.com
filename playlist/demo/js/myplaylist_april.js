49c7b29b9cd470f0bacc240cf8500d2ded9421ac
craneon <jennifer.friedmann@gmail.com>
2014-05-23 12:23:59 -0700

    April playlist

---
+++ playlist/demo/js/myplaylist_april.js
@@ -0,0 +1,233 @@
+/**
+ * Credit to CodeBaseHero for structure.
+ * april 2014 Playlist - JF Compilation
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/01+May.m4a',
+        title:'May',
+        artist:'towr',
+        buy:'#',
+        price:'New Haven',
+        duration:'3:09',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+        {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/02+Childs+Play+(feat.+Chance+the+Rapper).mp3',
+        title:'Childs Play (feat. Chance the Rapper)',
+        artist:'SZA',
+        buy:'#',
+        price:'SF',
+        duration:'3:37',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/03+Luck+Now.mp3',
+        title:'Luck Now',
+        artist:'Big Scary',
+        buy:'#',
+        price:'Seattle',
+        duration:'3:56',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/04+Ultima.mp3',
+        title:'Ultima',
+        artist:'Xenia Rubinos',
+        buy:'#',
+        price:'Asheville',
+        duration:'4:49',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+/**
+ *Track 5
+ */
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/05+Ragtime.mp3',
+        title:'Ragtime',
+        artist:'Neko Case',
+        buy:'#',
+        price:'New Haven',
+        duration:'5:16',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/06+Evolution.mp3',
+        title:'Evolution',
+        artist:'BFC',
+        buy:'#',
+        price:'New Haven',
+        duration:'6:51',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/07+Everything+Trying.mp3',
+        title:'Everything Trying',
+        artist:'Damien Jurado',
+        buy:'#',
+        price:'811 Page',
+        duration:'3:26',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/08+Echoes.mp3',
+        title:'Echoes',
+        artist:'Steve Lehman Octet',
+        buy:'#',
+        price:'Red Hook',
+        duration:'4:28',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/09+Lonely+Town.mp3',
+        title:'Lonely Town',
+        artist:'fLako',
+        buy:'#',
+        price:'Sausalito',
+        duration:'3:48',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+/**
+* Track 10
+*/
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/10+Dvor%CC%8Ca%CC%81k_+Symphony+%237+In+D+Minor%2C+Op.+70+-+2.+Poco+Adagio.m4a',
+        title:'Dvorak: Symphony #7 in D Minor',
+        artist:'Slovak Philharmonic Orchestra',
+        buy:'#',
+        price:'Brookline',
+        duration:'9:12',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/11+Scythian+Empire.mp3',
+        title:'Scythian Empire',
+        artist:'Andrew Bird', 
+        buy:'#',
+        price:'NYC',
+        duration:'6:53',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/12+Purple+Yellow+Red+and+Blue.mp3',
+        title:'Purple Yellow Red and Blue',
+        artist:'Portugal. The Man',
+        buy:'#',
+        price:'Silver Lake',
+        duration:'4:12',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/13+Delorean+Dynamite+(Disco+Mix).mp3',
+        title:'Delorean Dynamite (Disco Mix)',
+        artist:'Todd Terje',
+        buy:'#',
+        price:'New Haven',
+        duration:'6:41',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/14+Habits+(Stay+High).mp3',
+        title:'Habits (Stay High)',
+        artist:'Tove Lo',
+        buy:'#',
+        price:'LA',
+        duration:'3:28',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+/**
+* Track 15
+*/
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/15+Ma%27+Cheri.m4a',
+        title:'Ma&#8217 Cheri',
+        artist:'Freshlyground',
+        buy:'#',
+        price:'Harare',
+        duration:'4:31',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/16+10%2C000-Year+Earworm+to+Discourage+Settlement+Near+Nuclear+Waste+Repositories+(Don%27t+Change+Color%2C+Kitty).mp3',
+        title:'10000-Year Earworm to Discourage Settlement Near Nuclear Waste Repositories (Don&#8217t Change Color Kitty)',
+        artist:'Emperor X',
+        buy:'#',
+        price:'New Haven',
+        duration:'1:54',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/17+Son+Of+A+Gun+(Album+Version).mp3',
+        title:'Son Of A Gun',
+        artist:'Janet Jackson',
+        buy:'#',
+        price:'NYC',
+        duration:'5:58',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/18+Understand+At+All.mp3',
+        title:'Understand At All',
+        artist:'Cloud Nothings',
+        buy:'#',
+        price:'New Haven',
+        duration:'2:35',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/19+Danse+Carribe.mp3',
+        title:'Danse Carribe',
+        artist:'Andrew Bird',
+        buy:'#',
+        price:'Moscow',
+        duration:'5:19',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+/**Track 20*/
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/20+No+Excuse.mp3',
+        title:'No Excuse',
+        artist:'Jacques Greene',
+        buy:'#',
+        price:'New Haven',
+        duration:'5:38',
+        cover:'demo/mix/apriliv.jpg'
+    },
+
+    {
+        mp3:'http://d1zr06msg9w2vm.cloudfront.net/21+All+For+The+Best.mp3',
+        title:'All For The Best',
+        artist:'Thom Yorke',
+        buy:'#',
+        price:'Bed Stuy',
+        duration:'5:30',
+        cover:'demo/mix/apriliv.jpg'
+    }
+
+];
