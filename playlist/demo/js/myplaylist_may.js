af86a01559f1707def30ae94c3ff87eedd9c1987
craneon <jennifer.friedmann@gmail.com>
2014-07-06 14:49:22 -0700

    May Playlist

---
+++ playlist/demo/js/myplaylist_may.js
@@ -0,0 +1,223 @@
+/**
+ * Credit to CodeBaseHero for structure.
+ * april 2014 Playlist - JF Compilation
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/01+A+Little+Lost.mp3',
+        title:'A Little Lost',
+        artist:'Nat Baldwin',
+        buy:'#',
+        price:'Cafe 9',
+        duration:'3:24',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+        {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/02+Don%27t+Stop+%27Til+You+Get+Enough.mp3',
+        title:'Don&#8217t Stop &#8217Til You Get Enough',
+        artist:'Michael Jackson',
+        buy:'#',
+        price:'Santa Fe',
+        duration:'4:11',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/03+23+Floors+Up.m4a',
+        title:'23 Floors Up',
+        artist:'Teleman',
+        buy:'#',
+        price:'Cambridge',
+        duration:'3:55',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/04+Stolen+Dance.m4a',
+        title:'Stolen Dance',
+        artist:'Milky Chance',
+        buy:'#',
+        price:'Mount Kisco',
+        duration:'5:11',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+/**
+ *Track 5
+ */
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/05+A+minha+menina.mp3',
+        title:'A Minha Menina',
+        artist:'Os Mutantes',
+        buy:'#',
+        price:'D.C.',
+        duration:'4:46',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/06+Al+mar.mp3',
+        title:'Al Mar',
+        artist:'Manel',
+        buy:'#',
+        price:'Catalunya',
+        duration:'3:29',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/07+Sun+Star+Solar.mp3',
+        title:'Sun Star Solar',
+        artist:'Fatima',
+        buy:'#',
+        price:'Lower Haight',
+        duration:'1:15',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/08+Alone+In+Kyoto.mp3',
+        title:'Alone in Kyoto',
+        artist:'Air',
+        buy:'#',
+        price:'Kyoto',
+        duration:'4:49',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/09+The+Breach.mp3',
+        title:'The Breach',
+        artist:'Dustin Tebbutt',
+        buy:'#',
+        price:'Sydney',
+        duration:'4:16',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+/**
+* Track 10
+*/
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/10+The+Gathering+Sky.mp3',
+        title:'The Gathering Sky',
+        artist:'Pat Metheny',
+        buy:'#',
+        price:'NYC',
+        duration:'9:27',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/11+Ain%27t+That+A+Shame.mp3',
+        title:'Ain&#8217t That A Shame',
+        artist:'Fats Domino', 
+        buy:'#',
+        price:'Nob Hill',
+        duration:'2:24',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/12+Nenjam+Ellam.m4a',
+        title:'Nenjam Ellam',
+        artist:'A.R. Rahman',
+        buy:'#',
+        price:'Asheville',
+        duration:'5:25',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/13+Dirty+Sexy.mp3',
+        title:'Dirty Sexy',
+        artist:'Rusko',
+        buy:'#',
+        price:'LA',
+        duration:'6:41',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/14+Fuck+A+Bitch+Feat.+Death+Grips.mp3',
+        title:'Fuck A Bitch feat. Death Grips',
+        artist:'The Bug',
+        buy:'#',
+        price:'Sausalito',
+        duration:'3:05',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+/**
+* Track 15
+*/
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/15+Black+Albino+Bones.mp3',
+        title:'Black Albino Bones',
+        artist:'Fucked Up',
+        buy:'#',
+        price:'Chicago',
+        duration:'4:14',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/16+Letter+From+An+Occupant.m4a',
+        title:'Letter From An Occupant',
+        artist:'The New Pornographers',
+        buy:'#',
+        price:'Norwich',
+        duration:'3:48',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/17+Liebestraum+No.+3.m4a',
+        title:'Liebestraum No. 3',
+        artist:'Lizst',
+        buy:'#',
+        price:'Oxford',
+        duration:'4:11',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/18+JM.mp3',
+        title:'JM',
+        artist:'Strand of Oaks',
+        buy:'#',
+        price:'Tenderloin',
+        duration:'7:24',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/19+Alice+in+Wonderland.mp3',
+        title:'Alice in Wonderland',
+        artist:'The King&#8217s Singers',
+        buy:'#',
+        price:'Redmond',
+        duration:'2:28',
+        cover:'demo/mix/mayiv.jpg'
+    },
+
+/**Track 20*/
+    {
+        mp3:'http://d2xvtlmg27hewc.cloudfront.net/20+My+Song+5.m4a',
+        title:'My Song 5',
+        artist:'HAIM',
+        buy:'#',
+        price:'San Rafael',
+        duration:'3:53',
+        cover:'demo/mix/mayiv.jpg'
+    }
+
+];
