75150b8bcf2c286da53c3e7725a0dcc80f866f9f
craneon <jennifer.friedmann@gmail.com>
2014-09-04 13:18:12 -0700

    July Playlist

---
+++ playlist/demo/js/myplaylist_july.js
@@ -0,0 +1,222 @@
+/**
+ * Credit to CodeBaseHero for structure.
+ * July 2014 Playlist - JF Compilation
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'https://d3qx50a80i7p04.cloudfront.net/01+Fall+at+Your+Feet.mp3',
+        title:'Fall at Your Feet',
+        artist:'Boy & Bear',
+        buy:'#',
+        price:'NYC',
+        duration:'4:32',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/02+Young+Bride.mp3',
+        title:'Young Bride',
+        artist:'Midlake',
+        buy:'#',
+        price:'Paris',
+        duration:'4:39',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/03+Nigeru+Mono.mp3',
+        title:'Nigeru Mono',
+        artist:'Hirasawa Susumu',
+        buy:'#',
+        price:'New Haven',
+        duration:'4:57',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/04+Step+To+My+Girl.mp3',
+        title:'Step To My Girl',
+        artist:'Souls of Mischief',
+        buy:'#',
+        price:'Chestnut Hill',
+        duration:'4:39',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+/**
+ *Track 5
+ */
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/05+Take+Me+To+Church.mp3',
+        title:'Take Me To Church',
+        artist:'Hozier',
+        buy:'#',
+        price:'LGA-ORD',
+        duration:'3:01',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/06+%D0%9E%D0%BF%D0%B8%D1%83%D0%BC+%D0%B4%D0%BB%D1%8F+%D0%BD%D0%B8%D0%BA%D0%BE%D0%B3%D0%BE+(Opium+for+Nobody).mp3',
+        title:'Opium for Nobody',
+        artist:'Agatha Christie',
+        buy:'#',
+        price:'St. Petersburg',
+        duration:'3:57',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/07+Mass+Destruction.mp3',
+        title:'Mass Destruction',
+        artist:'Faithless',
+        buy:'#',
+        price:'D.C.',
+        duration:'3:44',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/08+The+Dream.m4a',
+        title:'The Dream',
+        artist:'Thee Oh Sees',
+        buy:'#',
+        price:'Pinecrest',
+        duration:'6:52',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/09+Nights+Off.mp3',
+        title:'Nights Off',
+        artist:'Siriusmo',
+        buy:'#',
+        price:'Las Vegas',
+        duration:'4:11',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+/**
+* Track 10
+*/
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/10+R.I.P.+Kevin+Miller.mp3',
+        title:'R.I.P. Kevin Miller',
+        artist:'Isaiah Rashad',
+        buy:'#',
+        price:'Oakland',
+        duration:'3:51',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/11+Hey+Life.mp3',
+        title:'Hey Life',
+        artist:'tUnE-yArDs', 
+        buy:'#',
+        price:'Chicago',
+        duration:'3:37',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/12+%E5%BD%A9%E8%99%B9+(Rainbow).mp3',
+        title:'Rainbow',
+        artist:'A-mei',
+        buy:'#',
+        price:'Singapore',
+        duration:'4:24',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/13+Line+of+Fire.mp3',
+        title:'Line of Fire',
+        artist:'Junip',
+        buy:'#',
+        price:'Cambridge',
+        duration:'5:39',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/14+I+Wanna+Go.mp3',
+        title:'I Wanna Go',
+        artist:'SUMMER HEART',
+        buy:'#',
+        price:'Windham',
+        duration:'4:08',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+/**
+* Track 15
+*/
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/15+Sitya+Loss.mp3',
+        title:'Sitya Loss',
+        artist:'Eddy Kenzo',
+        buy:'#',
+        price:'Boston',
+        duration:'4:25',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/16+%D0%A1%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D0%BE%D0%B5+%D0%A1%D0%B8%D1%8F%D0%BD%D0%B8%D0%B5.mp3',
+        title:'Aurora',
+        artist:'Ivan Dorn',
+        buy:'#',
+        price:'Hyderabad',
+        duration:'3:31',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/17+The+Truth+About+Cats+And+Dogs+(Is+That+They+Die).mp3',
+        title:'The Truth About Cats And Dogs (Is That They Die)',
+        artist:'Pony Up!',
+        buy:'#',
+        price:'Paris',
+        duration:'3:51',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/18+Ivory+Road.mp3',
+        title:'Ivory Road',
+        artist:'King Charles',
+        buy:'#',
+        price:'NYC',
+        duration:'3:53',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/19+Symphonia+IX+(My+Wait+Is+U).mp3',
+        title:'Symphonia IX (My Wait Is U)',
+        artist:'Grimes',
+        buy:'#',
+        price:'Chicago',
+        duration:'4:53',
+        cover:'demo/mix/julyiv.jpg'
+    },
+
+    {
+        mp3:'http://d3qx50a80i7p04.cloudfront.net/20+New+Slang.m4a',
+        title:'New Slang',
+        artist:'The Shins',
+        buy:'#',
+        price:'Oxford',
+        duration:'3:55',
+        cover:'demo/mix/julyiv.jpg'
+    }
+
+];
