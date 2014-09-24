915854488d17b94a6c0c7309fe6d12a2de657277
craneon <jennifer.friedmann@gmail.com>
2014-09-24 10:33:03 -0700

    August Playlist

---
+++ playlist/demo/js/myplaylist_august.js
@@ -0,0 +1,211 @@
+/**
+ * Credit to CodeBaseHero for structure.
+ * May 2014 Playlist - JF Compilation
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'https://d20ntj769xdqh0.cloudfront.net/01+Hey+Mami.mp3',
+        title:'Hey Mami',
+        artist:'Sylvan Esso',
+        buy:'#',
+        price:'SF',
+        duration:'3:20',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/02+Queen.mp3',
+        title:'Queen',
+        artist:'Perfume Genius',
+        buy:'#',
+        price:'SB',
+        duration:'3:52',
+        cover:'demo/mix/augustiv.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/03+Dance+Yrself+Clean.mp3',
+        title:'Dance Yrself Clean',
+        artist:'LCD Soundsystem',
+        buy:'#',
+        price:'London',
+        duration:'8:26',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/04+The+Fall.m4a',
+        title:'The Fall',
+        artist:'Rhye',
+        buy:'#',
+        price:'Cambridge',
+        duration:'3:45',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+/**
+ *Track 5
+ */
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/05+Misty.m4a',
+        title:'Misty',
+        artist:'Erroll Garner',
+        buy:'#',
+        price:'Seattle',
+        duration:'2:48',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/06+Another+Travelin%27+Song.mp3',
+        title:'Another Travelin&#8217 Song',
+        artist:'Bright Eyes',
+        buy:'#',
+        price:'HWY 1',
+        duration:'4:19',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/07+Blue+Eyes.mp3',
+        title:'Blue Eyes',
+        artist:'Destroyer',
+        buy:'#',
+        price:'NYC',
+        duration:'4:07',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/08+Happy+Idiot.mp3',
+        title:'Happy Idiot',
+        artist:'TV on The Radio',
+        buy:'#',
+        price:'CHI',
+        duration:'3:03',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/09+Plaistow+Flex+Out.mp3',
+        title:'Plaistow Flex Out',
+        artist:'Squarepusher',
+        buy:'#',
+        price:'Boston',
+        duration:'5:29',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+/**
+* Track 10
+*/
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/10+A%CC%81mame+Suavecito.m4a',
+        title:'Amame Suavecito',
+        artist:'Gilda',
+        buy:'#',
+        price:'Buenos Aires',
+        duration:'3:32',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/11+Easy+Come%2C+Easy+Go.mp3',
+        title:'Easy Come, Easy Go',
+        artist:'Diafrix', 
+        buy:'#',
+        price:'NYC',
+        duration:'3:19',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/12+Break+Free+(feat.+Zedd).mp3',
+        title:'Break Free',
+        artist:'Ariana Grande feat. Zedd',
+        buy:'#',
+        price:'LA Traffic',
+        duration:'3:35',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/13+Je+t%27emmene+au+vent.mp3',
+        title:'Je t&#8217emmene au vent',
+        artist:'Louise Attaque',
+        buy:'#',
+        price:'Paris',
+        duration:'3:06',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/14+He+Moved+Through+The+Fair.m4a',
+        title:'He Moved Through The Fair',
+        artist:'The Chieftains & Sinead O&#8217Connor',
+        buy:'#',
+        price:'NYC',
+        duration:'4:57',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+/**
+* Track 15
+*/
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/15+Fare+Thee+Well+(Dink%27s+Song).m4a',
+        title:'Fair Thee Well (Dink&#8217s Song)',
+        artist:'Oscar Issac',
+        buy:'#',
+        price:'Oakland',
+        duration:'2:48',
+        cover:'demo/mix/augustiv2.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/16+Chandelier+-+Sia+Cover.mp3',
+        title:'Chandelier - Sia Cover',
+        artist:'Sara Bareilles',
+        buy:'#',
+        price:'New Haven',
+        duration:'4:08',
+        cover:'demo/mix/augustiv.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/17+Mother+Whale+Eyeless.m4a',
+        title:'Mother Whale Eyeless',
+        artist:'Brian Eno',
+        buy:'#',
+        price:'Mansfield',
+        duration:'5:46',
+        cover:'demo/mix/augustiv3.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/18+Dreams.mp3',
+        title:'Dreams',
+        artist:'Wet',
+        buy:'#',
+        price:'Sestriere',
+        duration:'3:07',
+        cover:'demo/mix/augustiv5.jpg'
+    },
+
+    {
+        mp3:'http://d20ntj769xdqh0.cloudfront.net/19+Prehensile+Dream.mp3',
+        title:'Prehensile Dream',
+        artist:'Bad Plus',
+        buy:'#',
+        price:'Brooklyn',
+        duration:'8:13',
+        cover:'demo/mix/augustiv2.jpg'
+    }
+];
