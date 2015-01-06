014202b520e8856e3a82e1e66934333794d3e37a
craneon <jennifer.friedmann@gmail.com>
2015-01-06 14:14:50 -0800

    november playlist

---
+++ playlist/demo/js/myplaylist_november.js
@@ -0,0 +1,221 @@
+/**
+ * Credit to CodeBaseHero for structure.
+ * May 2014 Playlist - JF Compilation
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'https://dvs0dhm2pph6p.cloudfront.net/01+Now+That+I%27m+Older.mp3',
+        title:'Now That I&#8217m Older',
+        artist:'Sufjan Stevens',
+        buy:'#',
+        price:'Bed-Stuy',
+        duration:'4:56',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/02+Say+You+Do.mp3',
+        title:'Say You Do',
+        artist:'TV on the Radio',
+        buy:'#',
+        price:'SF',
+        duration:'2:16',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/03+This+Must+Be+the+Place+(Naive+Melody).mp3',
+        title:'This Must Be the Place (Naive Melody)',
+        artist:'Brothertiger',
+        buy:'#',
+        price:'LA',
+        duration:'5:28',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/04+Big+Lost.mp3',
+        title:'Big Lost',
+        artist:'Diplo',
+        buy:'#',
+        price:'Montreal',
+        duration:'4:39',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+/**
+ *Track 5
+ */
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/05+Losing+You.mp3',
+        title:'Losing You',
+        artist:'Solange',
+        buy:'#',
+        price:'Cambridge',
+        duration:'4:20',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/06+B2+-+Memory+1.mp3',
+        title:'B2 - Memory 1',
+        artist:'2030',
+        buy:'#',
+        price:'Cambridge',
+        duration:'3:35',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/07+Promise.mp3',
+        title:'Promise',
+        artist:'Ben Howard',
+        buy:'#',
+        price:'Cape Town',
+        duration:'6:24',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/08+Souls+by+TroyBoi+-+EDM.com+Exclusive.mp3',
+        title:'Souls',
+        artist:'TroyBoi',
+        buy:'#',
+        price:'Menlo Park',
+        duration:'3:25',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/09+Close+Your+Eyes+(And+Count+To+Fuck)+(Feat.+Zack+De+La+Rocha).mp3',
+        title:'Close Your Eyes (And Count To Fuck)',
+        artist:'Run The Jewels',
+        buy:'#',
+        price:'NJ',
+        duration:'3:54',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+/**
+* Track 10
+*/
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/10+Blank+Space.mp3',
+        title:'Blank Space',
+        artist:'Taylor Swift',
+        buy:'#',
+        price:'Santa Barbara',
+        duration:'3:52',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/11+Want+Your+Feeling.mp3',
+        title:'Want Your Feeling',
+        artist:'Jessie Ware', 
+        buy:'#',
+        price:'New Haven',
+        duration:'4:22',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/12+Woh+Ladki+Hai+Kahan.mp3',
+        title:'Woh Ladki Hai Kahan',
+        artist:'Kavita Subramanium Shaan',
+        buy:'#',
+        price:'NJ',
+        duration:'5:06',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/13+Decrescendo.mp3',
+        title:'Decrescendo',
+        artist:'Rx Bandits',
+        buy:'#',
+        price:'Oakland',
+        duration:'4:49',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/14+A+Kiss+to+Build+a+Dream+On.mp3',
+        title:'A Kiss to Build a Dream On',
+        artist:'Louis Armstrong',
+        buy:'#',
+        price:'Portland',
+        duration:'3:05',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+/**
+* Track 15
+*/
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/15+A%CC%80+tout+a%CC%80+l%27heure.mp3',
+        title:'A tout a l&#8217heure',
+        artist:'Bibio',
+        buy:'#',
+        price:'NYC',
+        duration:'4:10',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/16+Another+Love.mp3',
+        title:'Another Love',
+        artist:'Tom Odell',
+        buy:'#',
+        price:'Cambridge',
+        duration:'4:08',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/17+Says.mp3',
+        title:'Says',
+        artist:'Nils Frahm',
+        buy:'#',
+        price:'Baton Rouge',
+        duration:'5:10',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/18+Moonlight+in+Vermont.mp3',
+        title:'Moonlight in Vermont',
+        artist:'Ahmad Jamal',
+        buy:'#',
+        price:'Portland',
+        duration:'3:12',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/19+Adams_+The+Death+Of+Klinghoffer+-+Act+2_+Desert+Chorus.mp3',
+        title:'The Death of Klinghoffer - Act 2: Desert Chorus',
+        artist:'John Adams',
+        buy:'#',
+        price:'NYC',
+        duration:'5:05',
+        cover:'demo/mix/novemberiv.jpg'
+    },
+
+    {
+        mp3:'http://dvs0dhm2pph6p.cloudfront.net/20+Modern+Romance.mp3',
+        title:'Modern Romance',
+        artist:'TV on the Radio',
+        buy:'#',
+        price:'Big Sur',
+        duration:'4:12',
+        cover:'demo/mix/novemberiv.jpg'
+    }
+];
